8a2c975b4846704c5cb95425ff07c49dfafcb3c9
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2009-02-02 14:21:20 +0000

    added method for conveniently popping up a visualization

--- src/org/eclipse/imp/pdb/browser/ShowAction.java
+++ src/org/eclipse/imp/pdb/browser/ShowAction.java
@@ -15,22 +15,14 @@ package org.eclipse.imp.pdb.browser;
 import java.util.List;
 
 import org.eclipse.imp.pdb.analysis.AnalysisException;
-import org.eclipse.imp.pdb.facts.IValue;
 import org.eclipse.imp.pdb.facts.db.FactBase;
 import org.eclipse.imp.pdb.facts.db.IFactKey;
 import org.eclipse.imp.pdb.ui.graph.Editor;
 import org.eclipse.imp.pdb.ui.graph.GraphBuilder;
-import org.eclipse.imp.pdb.ui.graph.ValueEditorInput;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 
 public class ShowAction implements IViewActionDelegate {
     private FactBrowserView factBrowser;
@@ -45,28 +37,12 @@ public class ShowAction implements IViewActionDelegate {
         
         try {
         	for (IFactKey key : keys) {
-        		IValue value = factBase.getFact(key);
-        		openEditor(new ValueEditorInput(value));
+        		Editor.open(factBase.getFact(key));
         	}
         } catch (AnalysisException e) {
         	// TODO deal with this!
-        } catch (PartInitException e) {
-        	// TODO deal with this!
-		}
+        }
     }
-
-    private void openEditor(IEditorInput input) throws PartInitException {
-    	IWorkbench wb = PlatformUI.getWorkbench();
-		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-
-		if (win != null) {
-			IWorkbenchPage page = win.getActivePage();
-			
-			if (page != null) {
-				page.openEditor(input, Editor.EditorId);
-			}
-		}
-	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
     	boolean canShow = true;
